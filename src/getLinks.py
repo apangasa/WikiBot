d8fabf3d4934d92d258f064d623cafc7aa2675f7
apangasa <60531810+apangasa@users.noreply.github.com>
2020-05-26 15:57:06 -0400

    Get Links To/From Page

---
+++ src/getLinks.py
@@ -0,0 +1,49 @@
+import wikipedia
+import requests
+import time
+
+BASE_URL = "https://en.wikipedia.org/w/api.php/w/api.php?action=query&format=json&list=backlinks&bltitle="
+CONTINUE = "&blcontinue="
+MAX_RES = "&bllimit=max"
+FORMATTING = "&formatversion=2"
+
+
+def get_links_on_page(start_page):
+    return wikipedia.page(start_page).links
+
+
+def get_links_to_page(end_page):
+    full_results = []
+    more_results_available = True
+    first_run = True
+    continue_code = ""
+
+    while more_results_available:
+        call = BASE_URL + end_page + MAX_RES + FORMATTING
+
+        if not first_run:
+            call += CONTINUE + continue_code
+
+        r = requests.get(call)
+        data = r.json()
+
+        if data.get("continue"):
+            continue_code = data["continue"]["blcontinue"]
+        else:
+            more_results_available = False
+
+        results = data["query"]["backlinks"]
+
+        for i in range(len(results)):
+            full_results.append(results[i]["title"])
+
+        first_run = False
+
+    return full_results
+
+
+start = time.time()
+print(len(get_links_to_page("New York City")))
+end = time.time()
+
+print(end - start)
